3344f103473c71a1dabd9d6535f710fb9df16702
Sébastien Janaud <sebastien.janaud@gmail.com>
2013-03-17 13:16:15 +0000

    git-svn-id: http://svn.jensoft.org/trunk/jensoft-core@2365 83d0873e-991a-40c5-9a17-dc84bc7ed2a0

--- src/main/java/com/jensoft/core/desktop/scrollui/Sw2dScrollButton.java
+++ src/main/java/com/jensoft/core/desktop/scrollui/Sw2dScrollButton.java
@@ -15,61 +15,53 @@ import javax.swing.plaf.metal.MetalScrollButton;
 
 class Sw2dScrollButton extends MetalScrollButton {
 
-    private static Icon BUTTON_WEST;
-    private static Icon BUTTON_EAST;
-    private static Icon BUTTON_NORTH;
-    private static Icon BUTTON_SOUTH;
+	private static Icon BUTTON_WEST;
+	private static Icon BUTTON_EAST;
+	private static Icon BUTTON_NORTH;
+	private static Icon BUTTON_SOUTH;
 
-    static {
-        BUTTON_WEST = new ImageIcon(
-                                    Sw2dScrollbarUI.class
-                                            .getResource("scrollbar-button-west.png"));
-        BUTTON_EAST = new ImageIcon(
-                                    Sw2dScrollbarUI.class
-                                            .getResource("scrollbar-button-east.png"));
-        BUTTON_NORTH = new ImageIcon(
-                                     Sw2dScrollbarUI.class
-                                             .getResource("scrollbar-button-north.png"));
-        BUTTON_SOUTH = new ImageIcon(
-                                     Sw2dScrollbarUI.class
-                                             .getResource("scrollbar-button-south.png"));
-    }
+	static {
+		BUTTON_WEST = new ImageIcon(Sw2dScrollbarUI.class.getResource("scrollbar-button-west.png"));
+		BUTTON_EAST = new ImageIcon(Sw2dScrollbarUI.class.getResource("scrollbar-button-east.png"));
+		BUTTON_NORTH = new ImageIcon(Sw2dScrollbarUI.class.getResource("scrollbar-button-north.png"));
+		BUTTON_SOUTH = new ImageIcon(Sw2dScrollbarUI.class.getResource("scrollbar-button-south.png"));
+	}
 
-    Sw2dScrollButton(int direction, int width, boolean freeStanding) {
-        super(direction, width, freeStanding);
-        setOpaque(false);
-    }
+	Sw2dScrollButton(int direction, int width, boolean freeStanding) {
+		super(direction, width, freeStanding);
+		setOpaque(false);
+	}
 
-    @Override
-    public Dimension getMaximumSize() {
-        return getPreferredSize();
-    }
+	@Override
+	public Dimension getMaximumSize() {
+		return getPreferredSize();
+	}
 
-    @Override
-    public Dimension getMinimumSize() {
-        return getPreferredSize();
-    }
+	@Override
+	public Dimension getMinimumSize() {
+		return getPreferredSize();
+	}
 
-    @Override
-    public Dimension getPreferredSize() {
-        return new Dimension(17, 17);
-    }
+	@Override
+	public Dimension getPreferredSize() {
+		return new Dimension(17, 17);
+	}
 
-    @Override
-    public void paint(Graphics g) {
-        switch (getDirection()) {
-            case SwingConstants.WEST:
-                BUTTON_WEST.paintIcon(null, g, 0, 0);
-                break;
-            case SwingConstants.EAST:
-                BUTTON_EAST.paintIcon(null, g, 0, 0);
-                break;
-            case SwingConstants.NORTH:
-                BUTTON_NORTH.paintIcon(null, g, 0, 0);
-                break;
-            case SwingConstants.SOUTH:
-                BUTTON_SOUTH.paintIcon(null, g, 0, 0);
-                break;
-        }
-    }
+	@Override
+	public void paint(Graphics g) {
+		switch (getDirection()) {
+		case SwingConstants.WEST:
+			BUTTON_WEST.paintIcon(null, g, 0, 0);
+			break;
+		case SwingConstants.EAST:
+			BUTTON_EAST.paintIcon(null, g, 0, 0);
+			break;
+		case SwingConstants.NORTH:
+			BUTTON_NORTH.paintIcon(null, g, 0, 0);
+			break;
+		case SwingConstants.SOUTH:
+			BUTTON_SOUTH.paintIcon(null, g, 0, 0);
+			break;
+		}
+	}
 }
