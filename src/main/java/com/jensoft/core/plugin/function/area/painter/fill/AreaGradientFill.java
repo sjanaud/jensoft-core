1c8a46ddac8f119ce2c1eab7db52882a7d3bbb18
Sébastien Janaud <sebastien.janaud@gmail.com>
2013-04-14 15:58:58 +0000

    git-svn-id: http://svn.jensoft.org/trunk/jensoft-core@2508 83d0873e-991a-40c5-9a17-dc84bc7ed2a0

--- src/main/java/com/jensoft/core/plugin/function/area/painter/fill/AreaGradientFill.java
+++ src/main/java/com/jensoft/core/plugin/function/area/painter/fill/AreaGradientFill.java
@@ -15,6 +15,7 @@ import java.awt.geom.Rectangle2D;
 import com.jensoft.core.graphics.Shader;
 import com.jensoft.core.palette.ColorPalette;
 import com.jensoft.core.plugin.function.area.Area;
+import com.jensoft.core.plugin.function.source.FunctionNature;
 
 /***
  * define curve linear gradient fill<br>
@@ -23,185 +24,195 @@ import com.jensoft.core.plugin.function.area.Area;
  */
 public class AreaGradientFill extends AbstractAreaFill {
 
-    private Color color1;
-    private Color color2;
+	private Color color1;
+	private Color color2;
 
-    /** shade fractions */
-    private float[] shadeFractions;
+	/** shade fractions */
+	private float[] shadeFractions;
 
-    /** shade colors */
-    private Color[] shadeColors;
+	/** shade colors */
+	private Color[] shadeColors;
 
-    /**
-     * create default gradient fill
-     */
-    public AreaGradientFill() {
-    }
+	/**
+	 * create default gradient fill
+	 */
+	public AreaGradientFill() {
+	}
 
-    /**
-     * create area gradient with specified color
-     * 
-     * @param color1
-     * @param color2
-     */
-    public AreaGradientFill(Color color1, Color color2) {
-        super();
-        this.color1 = color1;
-        this.color2 = color2;
-    }
+	/**
+	 * create area gradient with specified color
+	 * 
+	 * @param color1
+	 * @param color2
+	 */
+	public AreaGradientFill(Color color1, Color color2) {
+		super();
+		this.color1 = color1;
+		this.color2 = color2;
+	}
 
-    /**
-     * create area gradient with specified parameters
-     * 
-     * @param shadeFractions
-     *            the shading fraction
-     * @param shadeColors
-     *            the shading color
-     */
-    public AreaGradientFill(float[] shadeFractions, Color[] shadeColors) {
-        super();
-        this.shadeFractions = shadeFractions;
-        this.shadeColors = shadeColors;
-    }
+	/**
+	 * create area gradient with specified parameters
+	 * 
+	 * @param shadeFractions
+	 *            the shading fraction
+	 * @param shadeColors
+	 *            the shading color
+	 */
+	public AreaGradientFill(float[] shadeFractions, Color[] shadeColors) {
+		super();
+		this.shadeFractions = shadeFractions;
+		this.shadeColors = shadeColors;
+	}
 
-    /**
-     * create area gradient with specified parameters
-     * 
-     * @param shader
-     *            the shader
-     */
-    public AreaGradientFill(Shader shader) {
-        super();
-        this.shadeFractions = shader.getFractions();
-        this.shadeColors = shader.getColors();
-    }
+	/**
+	 * create area gradient with specified parameters
+	 * 
+	 * @param shader
+	 *            the shader
+	 */
+	public AreaGradientFill(Shader shader) {
+		super();
+		this.shadeFractions = shader.getFractions();
+		this.shadeColors = shader.getColors();
+	}
 
-    /**
-     * @return the color1
-     */
-    public Color getColor1() {
-        return color1;
-    }
+	/**
+	 * @return the color1
+	 */
+	public Color getColor1() {
+		return color1;
+	}
 
-    /**
-     * @param color1
-     *            the color1 to set
-     */
-    public void setColor1(Color color1) {
-        this.color1 = color1;
-    }
+	/**
+	 * @param color1
+	 *            the color1 to set
+	 */
+	public void setColor1(Color color1) {
+		this.color1 = color1;
+	}
 
-    /**
-     * @return the color2
-     */
-    public Color getColor2() {
-        return color2;
-    }
+	/**
+	 * @return the color2
+	 */
+	public Color getColor2() {
+		return color2;
+	}
 
-    /**
-     * @param color2
-     *            the color2 to set
-     */
-    public void setColor2(Color color2) {
-        this.color2 = color2;
-    }
+	/**
+	 * @param color2
+	 *            the color2 to set
+	 */
+	public void setColor2(Color color2) {
+		this.color2 = color2;
+	}
 
-    /**
-     * @return the shadeFractions
-     */
-    public float[] getShadeFractions() {
-        return shadeFractions;
-    }
+	/**
+	 * @return the shadeFractions
+	 */
+	public float[] getShadeFractions() {
+		return shadeFractions;
+	}
 
-    /**
-     * @return the shadeColors
-     */
-    public Color[] getShadeColors() {
-        return shadeColors;
-    }
+	/**
+	 * @return the shadeColors
+	 */
+	public Color[] getShadeColors() {
+		return shadeColors;
+	}
 
-    /**
-     * set the shadow parameters
-     * 
-     * @param fractions
-     * @param colors
-     */
-    public void setShader(float[] fractions, Color[] colors) {
-        if (fractions.length != colors.length) {
-            throw new IllegalArgumentException("length array does not match");
-        }
-        shadeFractions = fractions;
-        shadeColors = colors;
-    }
+	/**
+	 * set the shadow parameters
+	 * 
+	 * @param fractions
+	 * @param colors
+	 */
+	public void setShader(float[] fractions, Color[] colors) {
+		if (fractions.length != colors.length) {
+			throw new IllegalArgumentException("length array does not match");
+		}
+		shadeFractions = fractions;
+		shadeColors = colors;
+	}
 
-    /**
-     * set the shader
-     * 
-     * @param shader
-     */
-    public void setShader(Shader shader) {
-        if (shader.getFractions().length != shader.getColors().length) {
-            throw new IllegalArgumentException("length array does not match");
-        }
-        shadeFractions = shader.getFractions();
-        shadeColors = shader.getColors();
-    }
+	/**
+	 * set the shader
+	 * 
+	 * @param shader
+	 */
+	public void setShader(Shader shader) {
+		if (shader.getFractions().length != shader.getColors().length) {
+			throw new IllegalArgumentException("length array does not match");
+		}
+		shadeFractions = shader.getFractions();
+		shadeColors = shader.getColors();
+	}
 
-    
-    /* (non-Javadoc)
-     * @see com.jensoft.core.plugin.function.area.painter.fill.AbstractAreaFill#paintAreaFill(java.awt.Graphics2D, com.jensoft.core.plugin.function.area.AreaFunction)
-     */
-    @Override
-    public final void paintAreaFill(Graphics2D g2d, Area area) {
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see com.jensoft.core.plugin.function.area.painter.fill.AbstractAreaFill#
+	 * paintAreaFill(java.awt.Graphics2D,
+	 * com.jensoft.core.plugin.function.area.AreaFunction)
+	 */
+	@Override
+	public final void paintAreaFill(Graphics2D g2d, Area area) {
 
-        // Shape shape = area.getAreaPath();
-        Shape shape = area.getArea();
-        if (shape == null) {
-            return;
-        }
+		// Shape shape = area.getAreaPath();
+		Shape shape = area.getArea();
+		if (shape == null) {
+			return;
+		}
 
-        Rectangle2D rect = shape.getBounds2D();
+		Rectangle2D rect = shape.getBounds2D();
 
-        LinearGradientPaint gradient = null;
+		LinearGradientPaint gradient = null;
 
-        if (color1 != null && color2 == null) {
-            color2 = color1;
-        }
+		if (color1 != null && color2 == null) {
+			color2 = color1;
+		}
 
-        if (color1 != null && color2 != null && shadeFractions == null) {
-            float[] f = { 0f, 1f };
-            Color[] c = { color1, color2 };
-            gradient = new LinearGradientPaint(new Point2D.Double(
-                                                                  rect.getCenterX(), rect.getMinY()),
-                                               new Point2D.Double(
-                                                                  rect.getCenterX(), rect.getMaxY()), f, c);
-        }
-        else if (shadeFractions != null && shadeColors != null
-                && shadeFractions.length == shadeColors.length) {
-            gradient = new LinearGradientPaint(new Point2D.Double(
-                                                                  rect.getCenterX(), rect.getMinY()),
-                                               new Point2D.Double(
-                                                                  rect.getCenterX(), rect.getMaxY()), shadeFractions,
-                                               shadeColors);
-        }
-        else {
-            float[] f = { 0f, 1f };
-            Color[] c = { area.getThemeColor(),
-                    ColorPalette.alpha(area.getThemeColor(), 80) };
-            Point2D start = new Point2D.Double(rect.getCenterX(),
-                                               rect.getMinY());
-            Point2D end = new Point2D.Double(rect.getCenterX(), rect.getMaxY());
-            if (!start.equals(end)) {
-                gradient = new LinearGradientPaint(start, end, f, c);
-            }
+		if (color1 != null && color2 != null && shadeFractions == null) {
+			float[] f = { 0f, 1f };
+			Color[] c = { color1, color2 };
 
-        }
+			if (area.getSourceFunction().getNature() == FunctionNature.XFunction) {
+				gradient = new LinearGradientPaint(new Point2D.Double(rect.getCenterX(), rect.getMinY()), new Point2D.Double(rect.getCenterX(), rect.getMaxY()), f, c);
+			} else {
+				gradient = new LinearGradientPaint(new Point2D.Double(rect.getMaxX(), rect.getCenterY()), new Point2D.Double(rect.getMinX(), rect.getCenterY()), f, c);
+			}
+		} else if (shadeFractions != null && shadeColors != null && shadeFractions.length == shadeColors.length) {
+			if (area.getSourceFunction().getNature() == FunctionNature.XFunction) {
+				gradient = new LinearGradientPaint(new Point2D.Double(rect.getCenterX(), rect.getMinY()), new Point2D.Double(rect.getCenterX(), rect.getMaxY()), shadeFractions, shadeColors);
+			} else {
+				gradient = new LinearGradientPaint(new Point2D.Double(rect.getMaxX(), rect.getCenterY()), new Point2D.Double(rect.getMinX(), rect.getCenterY()), shadeFractions, shadeColors);
+			}
 
-        if (gradient != null) {
-            g2d.setPaint(gradient);
+		} else {
+			float[] f = { 0f, 1f };
+			Color[] c = { area.getThemeColor(), ColorPalette.alpha(area.getThemeColor(), 80) };
+			if (area.getSourceFunction().getNature() == FunctionNature.XFunction) {
+				Point2D start = new Point2D.Double(rect.getCenterX(), rect.getMinY());
+				Point2D end = new Point2D.Double(rect.getCenterX(), rect.getMaxY());
+				if (!start.equals(end)) {
+					gradient = new LinearGradientPaint(start, end, f, c);
+				}
+			}else{
+				Point2D start = new Point2D.Double(rect.getMaxX(), rect.getCenterY());
+				Point2D end = new Point2D.Double(rect.getMinX(), rect.getCenterY());
+				if (!start.equals(end)) {
+					gradient = new LinearGradientPaint(start, end, f, c);
+				}
+			}
+			
 
-            g2d.fill(area.getArea());
-        }
-    }
+		}
+
+		if (gradient != null) {
+			g2d.setPaint(gradient);
+
+			g2d.fill(area.getArea());
+		}
+	}
 
 }
