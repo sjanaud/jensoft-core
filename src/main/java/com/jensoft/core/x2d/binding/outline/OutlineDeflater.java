941db5f460a59bc41c9f99541ae772d65919d0e8
seb <seb@mycompany.com>
2013-04-19 22:57:34 +0000

    git-svn-id: http://svn.jensoft.org/trunk/jensoft-core@2566 83d0873e-991a-40c5-9a17-dc84bc7ed2a0

---
+++ src/main/java/com/jensoft/core/x2d/binding/outline/OutlineDeflater.java
@@ -0,0 +1,29 @@
+package com.jensoft.core.x2d.binding.outline;
+
+import org.w3c.dom.Element;
+
+import com.jensoft.core.plugin.outline.OutlinePlugin;
+import com.jensoft.core.x2d.binding.AbstractX2DPluginDeflater;
+
+public class OutlineDeflater extends AbstractX2DPluginDeflater<OutlinePlugin> {
+	
+	
+
+	public OutlineDeflater() {
+		super();
+	}
+
+	public OutlineDeflater(OutlinePlugin plugin) {
+		super(plugin);
+	}
+
+	/* (non-Javadoc)
+	 * @see com.jensoft.core.x2d.binding.AbstractX2DPluginDeflater#deflate()
+	 */
+	@Override
+	public Element deflate() {
+		
+		return null;
+	}
+
+}
