94c3ab5d8e353500b7d8ddc05540277f9a42a050
Sébastien Janaud <sebastien.janaud@gmail.com>
2013-04-19 23:36:02 +0000

    git-svn-id: http://svn.jensoft.org/trunk/jensoft-core@2571 83d0873e-991a-40c5-9a17-dc84bc7ed2a0

--- src/main/java/com/jensoft/core/x2d/binding/outline/OutlineDeflater.java
+++ src/main/java/com/jensoft/core/x2d/binding/outline/OutlineDeflater.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) JenSoft API
+ * This source file is part of JenSoft API, All rights reserved.
+ * JENSOFT PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
 package com.jensoft.core.x2d.binding.outline;
 
 import org.w3c.dom.Element;
@@ -5,6 +10,11 @@ import org.w3c.dom.Element;
 import com.jensoft.core.plugin.outline.OutlinePlugin;
 import com.jensoft.core.x2d.binding.AbstractX2DPluginDeflater;
 
+/**
+ * <code>OutlineDeflater</code>
+ * @author sebastien janaud
+ *
+ */
 public class OutlineDeflater extends AbstractX2DPluginDeflater<OutlinePlugin>  implements X2DOutlineElement{		
 
 	public OutlineDeflater() {
