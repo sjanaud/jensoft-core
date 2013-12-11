7e4c1bf8fe9d2e5796e46cf6ca297f9c31c3be8d
Sébastien Janaud <sebastien.janaud@gmail.com>
2013-12-11 18:21:08 +0000

    comment
    git-svn-id: http://svn.jensoft.org/trunk/jensoft-core@3668 83d0873e-991a-40c5-9a17-dc84bc7ed2a0

--- src/main/java/com/jensoft/core/plugin/stock/geom/BollingerMovingAverageStockGeom.java
+++ src/main/java/com/jensoft/core/plugin/stock/geom/BollingerMovingAverageStockGeom.java
@@ -12,11 +12,49 @@ import com.jensoft.core.plugin.function.source.UserSourceFunction;
 import com.jensoft.core.plugin.stock.Stock;
 
 /**
- * <code>BollingerMovingAverageStockGeom</code> defines a Bollinger band's
- * for a set of stock items.
+ * <code>BollingerMovingAverageStockGeom</code> defines a Bollinger band's for a
+ * set of stock items.
  * <p>
- * move count property define the moving average session count.
- * </p>
+ * 
+ * Assume a 5 bar Bollinger band with 2 Deviations, and assume the last five
+ * closes were 25.5, 26.75, 27.0, 26.5, and 27.25.<br>
+ * 
+ * <br>
+ * <br>
+ * Calculate the simple moving average: <br>
+ * <br>
+ * 25.5 + 26.75 + 27.0 + 26.5 + 27.25 = 133.0 <br>
+ * <br>
+ * 133.0 / 5 = 26.6 <br>
+ * <br>
+ * Next, for each bar, subtract 26.6 from the close and square this value: <br>
+ * <br>
+ * 25.5 - 26.6 = -1.1 squared = 1.21<br>
+ * <br>
+ * 26.75 - 26.6 = 0.15 squared = 0.023<br>
+ * <br>
+ * 27.0 - 26.6 = 0.4 squared = 0.16<br>
+ * <br>
+ * 26.5 - 26.6 = 0.1 squared = 0.01<br>
+ * <br>
+ * 27.25 - 26.6 = 0.65 squared = 0.423<br>
+ * <br>
+ * <br>
+ * Add the above calculated values, divide by 5, and then get the square root of
+ * this value to get the deviation value: <br>
+ * <br>
+ * 1.21 + 0.023 + 0.16 + 0.01 + 0.423 = 1.826 <br>
+ * <br>
+ * 1.826 / 5 = 0.365 <br>
+ * <br>
+ * Square root of .365 = 0.604 <br>
+ * <br>
+ * The upper Bollinger band would be 26.6 + (2 * 0.604) = 27.808 <br>
+ * <br>
+ * The middle Bollinger band would be 26.6 <br>
+ * <br>
+ * The lower Bollinger band would be 26.6 - (2 * 0.604) = 25.392 <br>
+ * <br>
  * 
  * @since 1.0
  * @author sebastien janaud
@@ -26,10 +64,10 @@ public class BollingerMovingAverageStockGeom extends CurveStockGeom {
 
 	/** up bollinger curve metrics path */
 	private MetricsPathFunction pathFunctionUp;
-	
+
 	/** bottom bollinger curve metrics path */
 	private MetricsPathFunction pathFunctionBottom;
-	
+
 	/** move count */
 	private int moveCount = 20;
 
@@ -44,6 +82,7 @@ public class BollingerMovingAverageStockGeom extends CurveStockGeom {
 
 	/**
 	 * create moving geometry with given average count
+	 * 
 	 * @param moveCount
 	 */
 	public BollingerMovingAverageStockGeom(int moveCount) {
@@ -90,10 +129,10 @@ public class BollingerMovingAverageStockGeom extends CurveStockGeom {
 		List<Point2D> stockMAs = new ArrayList<Point2D>();
 		List<Stock> stocks = getLayer().getHost().getStocks();
 		Collections.sort(stocks, new StockComparator());
-		
+
 		for (int i = moveCount; i < stocks.size(); i++) {
 			Stock root = stocks.get(i);
-			
+
 			double sum = 0;
 			for (int j = 0; j < moveCount; j++) {
 				Stock s = stocks.get(i - j);
@@ -101,32 +140,31 @@ public class BollingerMovingAverageStockGeom extends CurveStockGeom {
 			}
 			double movingAverage = sum / moveCount;
 			stockMAs.add(new Point2D.Double(new Long(root.getFixing().getTime()).doubleValue(), movingAverage));
-			
+
 			double squarred = 0;
 			for (int j = 0; j < moveCount; j++) {
 				Stock s = stocks.get(i - j);
-				squarred = squarred + Math.pow((s.getClose()-movingAverage), 2);
+				squarred = squarred + Math.pow((s.getClose() - movingAverage), 2);
 			}
-			double deviation = Math.sqrt(squarred/new Double(moveCount).doubleValue());
-			upperPoint.add(new Point2D.Double(new Long(root.getFixing().getTime()).doubleValue(), movingAverage + 2*deviation));
-			bottomPoint.add(new Point2D.Double(new Long(root.getFixing().getTime()).doubleValue(), movingAverage - 2*deviation));
+			double deviation = Math.sqrt(squarred / new Double(moveCount).doubleValue());
+			upperPoint.add(new Point2D.Double(new Long(root.getFixing().getTime()).doubleValue(), movingAverage + 2 * deviation));
+			bottomPoint.add(new Point2D.Double(new Long(root.getFixing().getTime()).doubleValue(), movingAverage - 2 * deviation));
 		}
-		
+
 		SourceFunction averageSourceFunction = new UserSourceFunction.LineSource(stockMAs);
 		getPathFunction().setSource(averageSourceFunction);
-		
+
 		SourceFunction upperSourceFunction = new UserSourceFunction.LineSource(upperPoint);
 		getPathFunctionUp().setSource(upperSourceFunction);
-		
+
 		SourceFunction bottomSourceFunction = new UserSourceFunction.LineSource(bottomPoint);
 		getPathFunctionBottom().setSource(bottomSourceFunction);
-		
+
 	}
-	
-	
 
 	/**
 	 * get bollinger up curve
+	 * 
 	 * @return up curve
 	 */
 	public MetricsPathFunction getPathFunctionUp() {
@@ -135,12 +173,11 @@ public class BollingerMovingAverageStockGeom extends CurveStockGeom {
 
 	/**
 	 * get bollinger bottonm curve
+	 * 
 	 * @return bottom curve
 	 */
 	public MetricsPathFunction getPathFunctionBottom() {
 		return pathFunctionBottom;
 	}
-	
-	
 
 }
