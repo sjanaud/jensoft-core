6f4d046c686a68121b9186a93e75deb1e61ec8b3
Sébastien Janaud <sebastien.janaud@gmail.com>
2013-12-22 17:16:33 +0000

    clean code
    git-svn-id: http://svn.jensoft.org/trunk/jensoft-core@3741 83d0873e-991a-40c5-9a17-dc84bc7ed2a0

--- src/main/java/com/jensoft/core/plugin/gauge/compass/CompassBody.java
+++ src/main/java/com/jensoft/core/plugin/gauge/compass/CompassBody.java
@@ -10,10 +10,6 @@ import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics2D;
-import java.awt.Shape;
-import java.awt.font.FontRenderContext;
-import java.awt.font.GlyphVector;
-import java.awt.geom.AffineTransform;
 import java.awt.geom.Arc2D;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
@@ -22,10 +18,8 @@ import java.util.List;
 import com.jensoft.core.device.PartBuffer;
 import com.jensoft.core.glyphmetrics.AbstractMetricsPath.ProjectionNature;
 import com.jensoft.core.glyphmetrics.GeneralMetricsPath;
-import com.jensoft.core.glyphmetrics.GeometryPath;
 import com.jensoft.core.glyphmetrics.GlyphMetric;
 import com.jensoft.core.glyphmetrics.GlyphMetricsNature;
-import com.jensoft.core.glyphmetrics.GlyphUtil;
 import com.jensoft.core.glyphmetrics.StylePosition;
 import com.jensoft.core.glyphmetrics.painter.fill.GlyphFill;
 import com.jensoft.core.glyphmetrics.painter.marker.TriangleMarker;
@@ -33,11 +27,16 @@ import com.jensoft.core.glyphmetrics.painter.marker.TriangleMarker.TriangleDirec
 import com.jensoft.core.palette.ColorPalette;
 import com.jensoft.core.palette.InputFonts;
 import com.jensoft.core.palette.NanoChromatique;
-import com.jensoft.core.palette.PetalPalette;
 import com.jensoft.core.palette.TangoPalette;
 import com.jensoft.core.plugin.gauge.RadialGauge;
 import com.jensoft.core.plugin.gauge.core.BodyGaugePainter;
 
+/**
+ * <code>CompassBody</code>
+ * 
+ * @author sebastien janaud
+ * 
+ */
 public class CompassBody extends BodyGaugePainter {
 
 	private GeneralMetricsPath pathManagerLabel;
@@ -45,11 +44,13 @@ public class CompassBody extends BodyGaugePainter {
 	private GeneralMetricsPath pathManagerNeedle;
 
 	private double capDegree = 90;
-	private int startAngleDegreee = 0;
-	private int endAngleDegree = 360;
+	
 	private PartBuffer metricsPart;
 	private SailCompassTick compass;
 
+	/**
+	 * create compass body
+	 */
 	public CompassBody() {
 
 		pathManager = new GeneralMetricsPath();
@@ -65,7 +66,7 @@ public class CompassBody extends BodyGaugePainter {
 		pathManagerLabel.setReverseAll(true);
 		pathManagerLabel.setMin(0);
 		pathManagerLabel.setMax(360);
-		
+
 		pathManagerNeedle = new GeneralMetricsPath();
 		pathManagerNeedle.setProjectionNature(ProjectionNature.DEVICE);
 		pathManagerNeedle.setAutoReverseGlyph(false);
@@ -77,25 +78,19 @@ public class CompassBody extends BodyGaugePainter {
 		createLabel2();
 		createCompasse();
 	}
-	
-	
 
 	public double getCapDegree() {
 		return capDegree;
 	}
 
-
-
 	public void setCapDegree(double capDegree) {
 		this.capDegree = capDegree;
 	}
 
-
-
 	private void createLabel2() {
 		GlyphMetric metric;
 		Font f = InputFonts.getFont(InputFonts.ELEMENT, 34);
-		
+
 		metric = new GlyphMetric();
 		metric.setValue(0);
 		metric.setStylePosition(StylePosition.Default);
@@ -106,7 +101,7 @@ public class CompassBody extends BodyGaugePainter {
 		metric.setFont(f);
 		pathManager.addMetric(metric);
 
-		//north
+		// north
 		metric = new GlyphMetric();
 		metric.setValue(90);
 		metric.setStylePosition(StylePosition.Default);
@@ -117,7 +112,7 @@ public class CompassBody extends BodyGaugePainter {
 		metric.setFont(f);
 		pathManager.addMetric(metric);
 
-		//west
+		// west
 		metric = new GlyphMetric();
 		metric.setValue(180);
 		metric.setStylePosition(StylePosition.Default);
@@ -128,7 +123,7 @@ public class CompassBody extends BodyGaugePainter {
 		metric.setFont(f);
 		pathManager.addMetric(metric);
 
-		//south
+		// south
 		metric = new GlyphMetric();
 		metric.setValue(270);
 		metric.setStylePosition(StylePosition.Default);
@@ -144,7 +139,7 @@ public class CompassBody extends BodyGaugePainter {
 
 		GlyphMetric metric;
 		Font f = InputFonts.getFont(InputFonts.ELEMENT, 12);
-		
+
 		metric = new GlyphMetric();
 		metric.setValue(30);
 		metric.setStylePosition(StylePosition.Tangent);
@@ -226,7 +221,6 @@ public class CompassBody extends BodyGaugePainter {
 		pathManagerLabel.addMetric(metric);
 	}
 
-
 	public void createCompasse() {
 
 		compass = new SailCompassTick(0, 0, 150);
@@ -257,6 +251,11 @@ public class CompassBody extends BodyGaugePainter {
 
 	}
 
+	
+	/**
+	 * paint compass ticker
+	 * @param g2d
+	 */
 	protected void paintCompass(Graphics2D g2d) {
 
 		double centerX = getGauge().getWindow2D().userToPixel(new Point2D.Double(getGauge().getX(), 0)).getX();
@@ -267,31 +266,21 @@ public class CompassBody extends BodyGaugePainter {
 		compass.setCenterY((int) centerY);
 		compass.setBaseRadius(radius - 30);
 		compass.builCompass();
-		Shape baseShape = compass.getBaseShape();
-		// g2d.setColor(getWindow2D().getThemeColor().darker());
-
-		// g2d.setColor(new Color(255,255,255,50));
-		// g2d.fill(compass.getBaseShape());
+		
 
 		g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 0.6f));
 
-		// g2d.fill(baseShape);
 		g2d.setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, 1f));
 		List<CompassCapTicker> needles = compass.getNeedles();
-		// g2d.setStroke(new BasicStroke(0.5f));
 
 		g2d.setColor(getGauge().getWindow2D().getThemeColor().darker());
 		g2d.setStroke(new BasicStroke(1f));
 		Color blue = new Color(68, 155, 180);
-		Color green = new Color(162, 189, 66);
-		// Color orange = new Color(237,162,44);
-		Color orange = PetalPalette.PETAL2_HC;
+		
 
 		for (CompassCapTicker n : needles) {
 
 			Line2D needlePath = n.getNeedlePath();
-
-			// g2d.setColor(Color.WHITE);
 
 			if (n.getNature() == CompassCapTicker.MAJOR) {
 				g2d.setStroke(new BasicStroke(1.5f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND));
@@ -311,126 +300,28 @@ public class CompassBody extends BodyGaugePainter {
 			}
 
 			g2d.draw(needlePath);
-
-			GeometryPath geoPath = new GeometryPath(n.getBaseLine());
-			GeometryPath geoPath2 = new GeometryPath(n.getNeedleArc());
-			int offset = 10;
-			AffineTransform af = new AffineTransform();
-			AffineTransform af2 = new AffineTransform();
-			if (n.getTheta() == 0 || n.getTheta() == 90 || n.getTheta() == 180 || n.getTheta() == 270) {
-
-				String cardinal = null;
-				String cap = null;
-				if (n.getTheta() == 0) {
-					cardinal = "East";
-					cap = "90�";
-				} else if (n.getTheta() == 90) {
-					cardinal = "North";
-					cap = "0�";
-					// g2d.draw(n.getNeedleArc());
-					// //g2d.draw(n.getBaseLine());
-					// g2d.draw(n.getNeedlePath());
-					//
-					// Ellipse2D e = new
-					// Ellipse2D.Double(n.getRefPoint().getX()-1,n.getRefPoint().getY()-1,2,2);
-					// g2d.draw(e);
-				} else if (n.getTheta() == 180) {
-					cardinal = "West";
-					cap = "270�";
-				} else if (n.getTheta() == 270) {
-					cardinal = "South";
-					cap = "180�";
-
-				}
-
-				// g2d.draw(n.getBaseLine());
-				Font f = new Font("Tahoma", Font.PLAIN, 14);
-				Font f2 = new Font("Tahoma", Font.PLAIN, 14);
-				FontRenderContext frc = g2d.getFontRenderContext();
-				GlyphVector glyphVector = f.createGlyphVector(frc, cardinal);
-				GlyphVector glyphVector2 = f2.createGlyphVector(frc, cap);
-
-				float gv2Width = GlyphUtil.getGlyphWidth(glyphVector2);
-				float angleOffset = geoPath2.lengthOfPath() / 2 - gv2Width / 2;
-
-				for (int j = 0; j < glyphVector2.getNumGlyphs(); j++) {
-
-					Point2D p = glyphVector2.getGlyphPosition(j);
-					float px = (float) p.getX();
-					float py = (float) p.getY();
-
-					Point2D pointGlyph = geoPath2.pointAtLength(angleOffset + GlyphUtil.getGlyphWidthAtToken(glyphVector2, j));
-
-					Shape glyph = glyphVector2.getGlyphOutline(j);
-					// Shape glyphBound2D =
-					// glyphVector2.getGlyphOutline(j).getBounds2D();
-
-					float angle = geoPath2.angleAtLength(angleOffset + GlyphUtil.getGlyphWidthAtToken(glyphVector2, j));
-					af2.setToTranslation(pointGlyph.getX(), pointGlyph.getY());
-					af2.rotate(angle);
-					af2.translate(-px, -py + 4);
-
-					Shape ts = af2.createTransformedShape(glyph);
-
-					g2d.setColor(Color.orange);
-					// Shape tsBound2D =
-					// af2.createTransformedShape(glyphBound2D);
-					// g2d.draw(ts);
-
-					g2d.setColor(getGauge().getWindow2D().getThemeColor());
-
-					// item.addGlyphOutline(ts);
-					// g2d.fill(ts);
-
-				}
-
-				for (int j = 0; j < glyphVector.getNumGlyphs(); j++) {
-
-					Point2D p = glyphVector.getGlyphPosition(j);
-					float px = (float) p.getX();
-					float py = (float) p.getY();
-
-					Point2D pointGlyph = geoPath.pointAtLength(offset + GlyphUtil.getGlyphWidthAtToken(glyphVector, j));
-
-					Shape glyph = glyphVector.getGlyphOutline(j);
-					// Shape glyphBound2D =
-					// glyphVector.getGlyphOutline(j).getBounds2D();
-
-					float angle = geoPath.angleAtLength(offset + GlyphUtil.getGlyphWidthAtToken(glyphVector, j));
-					af.setToTranslation(pointGlyph.getX(), pointGlyph.getY());
-					af.rotate(angle);
-					af.translate(-px, -py + 4);
-
-					Shape ts = af.createTransformedShape(glyph);
-
-					g2d.setColor(Color.orange);
-					// Shape tsBound2D =
-					// af.createTransformedShape(glyphBound2D);
-					// g2d.draw(ts);
-
-					g2d.setColor(getGauge().getWindow2D().getThemeColor());
-
-					// item.addGlyphOutline(ts);
-					// g2d.fill(ts);
-
-				}
-
-			}
-
 		}
 
 	}
 
+	
+	/**
+	 * paint metrics
+	 * @param g2d
+	 */
 	private void paintMetrics(Graphics2D g2d) {
 
 		double centerX = getGauge().getWindow2D().userToPixel(new Point2D.Double(getGauge().getX(), 0)).getX();
 		double centerY = getGauge().getWindow2D().userToPixel(new Point2D.Double(0, getGauge().getY())).getY();
 
+		int startAngleDegreee = 0;
+		int extendsDegree = 360;
+		
 		int radius1 = getGauge().getRadius() - 10;
-		Arc2D arc2d = new Arc2D.Double(centerX - radius1, centerY - radius1, 2 * radius1, 2 * radius1, startAngleDegreee, endAngleDegree - startAngleDegreee, Arc2D.OPEN);
+		Arc2D arc2d = new Arc2D.Double(centerX - radius1, centerY - radius1, 2 * radius1, 2 * radius1, startAngleDegreee, extendsDegree, Arc2D.OPEN);
 
 		int radius2 = getGauge().getRadius() - 50;
-		Arc2D arc2d2 = new Arc2D.Double(centerX - radius2, centerY - radius2, 2 * radius2, 2 * radius2, startAngleDegreee, endAngleDegree - startAngleDegreee, Arc2D.OPEN);
+		Arc2D arc2d2 = new Arc2D.Double(centerX - radius2, centerY - radius2, 2 * radius2, 2 * radius2, startAngleDegreee, extendsDegree, Arc2D.OPEN);
 
 		int radius = getGauge().getRadius();
 		pathManager.setWindow2d(getGauge().getWindow2D());
@@ -438,30 +329,25 @@ public class CompassBody extends BodyGaugePainter {
 		pathManager.setFontRenderContext(g2d.getFontRenderContext());
 		pathManager.append(arc2d);
 		pathManager.setSolveGeometryRequest(true);
-		
+
 		pathManagerLabel.setWindow2d(getGauge().getWindow2D());
 		pathManagerLabel.resetPath();
 		pathManagerLabel.setFontRenderContext(g2d.getFontRenderContext());
 		pathManagerLabel.append(arc2d2);
 		pathManagerLabel.setSolveGeometryRequest(true);
-		
-		
-		
-		
+
 		g2d.setColor(Color.RED);
-		//g2d.draw(arc2d2);
-		
-		
+		// g2d.draw(arc2d2);
+
 		if (metricsPart == null) {
 
 			metricsPart = new PartBuffer(centerX - radius, centerY - radius, 2 * radius, 2 * radius);
-			
+
 			Graphics2D g2dPart = metricsPart.getBuffer().createGraphics();
 			g2dPart.setRenderingHints(g2d.getRenderingHints());
 			g2dPart.translate(-centerX + radius, -centerY + radius);
 			g2dPart.setStroke(new BasicStroke(0.4f));
 			g2dPart.setColor(Color.BLACK);
-
 
 			List<GlyphMetric> metrics = pathManager.getMetrics();
 			for (GlyphMetric m : metrics) {
@@ -508,38 +394,35 @@ public class CompassBody extends BodyGaugePainter {
 		}
 
 	}
-
-	@Override
-	public void paintBody(Graphics2D g2d, RadialGauge radialGauge) {
-		paintCompass(g2d);
-		paintMetrics(g2d);
-		paintNeedle(g2d);
-	}
-
-    public void paintNeedle(Graphics2D g2d) {
-    	double centerX = getGauge().getWindow2D().userToPixel(new Point2D.Double(getGauge().getX(), 0)).getX();
+	
+	public void paintNeedle(Graphics2D g2d) {
+		double centerX = getGauge().getWindow2D().userToPixel(new Point2D.Double(getGauge().getX(), 0)).getX();
 		double centerY = getGauge().getWindow2D().userToPixel(new Point2D.Double(0, getGauge().getY())).getY();
+
 		
-    	int radius2 = getGauge().getRadius() - 80;
-		Arc2D arc2d2 = new Arc2D.Double(centerX - radius2, centerY - radius2, 2 * radius2, 2 * radius2, startAngleDegreee, endAngleDegree - startAngleDegreee, Arc2D.OPEN);
+		int startAngleDegreee = 0;
+		int extendsDegree = 360;
 		
+		int radius2 = getGauge().getRadius() - 80;
+		Arc2D arc2d2 = new Arc2D.Double(centerX - radius2, centerY - radius2, 2 * radius2, 2 * radius2, startAngleDegreee,extendsDegree, Arc2D.OPEN);
+
 		pathManagerNeedle.clearMetric();
-    	pathManagerNeedle.setWindow2d(getGauge().getWindow2D());
+		pathManagerNeedle.setWindow2d(getGauge().getWindow2D());
 		pathManagerNeedle.resetPath();
 		pathManagerNeedle.setFontRenderContext(g2d.getFontRenderContext());
 		pathManagerNeedle.append(arc2d2);
 		pathManagerNeedle.setSolveGeometryRequest(true);
-    	
-    	GlyphMetric metric = new GlyphMetric();
+
+		GlyphMetric metric = new GlyphMetric();
 		metric.setValue(capDegree);
-		
-		//triangle marker
-		TriangleMarker triangle = new TriangleMarker(Color.WHITE,NanoChromatique.BLUE,15,20);
+
+		// triangle marker
+		TriangleMarker triangle = new TriangleMarker(Color.WHITE, NanoChromatique.BLUE, 15, 20);
 		triangle.setDirection(TriangleDirection.In);
 		metric.setGlyphMetricMarkerPainter(triangle);
-		
+
 		pathManagerNeedle.addMetric(metric);
-	
+
 		pathManagerNeedle.setFontRenderContext(g2d.getFontRenderContext());
 		pathManagerNeedle.setSolveGeometryRequest(true);
 		List<GlyphMetric> metrics2 = pathManagerNeedle.getMetrics();
@@ -550,6 +433,19 @@ public class CompassBody extends BodyGaugePainter {
 			}
 
 		}
-    }
+	}
+	
+
+	/* (non-Javadoc)
+	 * @see com.jensoft.core.plugin.gauge.core.BodyGaugePainter#paintBody(java.awt.Graphics2D, com.jensoft.core.plugin.gauge.RadialGauge)
+	 */
+	@Override
+	public void paintBody(Graphics2D g2d, RadialGauge radialGauge) {
+		paintCompass(g2d);
+		paintMetrics(g2d);
+		paintNeedle(g2d);
+	}
+
+	
 
 }
