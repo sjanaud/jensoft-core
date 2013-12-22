544c0b051d1f595b688f1932611cfe9fdeb5372e
Sébastien Janaud <sebastien.janaud@gmail.com>
2013-12-22 17:49:39 +0000

    gauge part buffer
    git-svn-id: http://svn.jensoft.org/trunk/jensoft-core@3742 83d0873e-991a-40c5-9a17-dc84bc7ed2a0

---
+++ src/main/java/com/jensoft/core/plugin/gauge/core/GaugePartBuffer.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) JenSoft API
+ * This source file is part of JenSoft API, All rights reserved.
+ * JENSOFT PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.jensoft.core.plugin.gauge.core;
+
+import java.awt.Graphics2D;
+import java.awt.geom.Point2D;
+
+import com.jensoft.core.device.PartBuffer;
+import com.jensoft.core.plugin.gauge.RadialGauge;
+
+/**
+ * <code>GaugePartBuffer</code>
+ * 
+ * @author sebastien janaud
+ * 
+ */
+public class GaugePartBuffer extends PartBuffer {
+
+	private Graphics2D g2dPart;
+
+	/**
+	 * create a gauge part buffer to be reuse
+	 * @param gauge
+	 */
+	public GaugePartBuffer(RadialGauge gauge) {
+		super(gauge.getWindow2D().userToPixel(new Point2D.Double(gauge.getX(), 0)).getX(), gauge.getWindow2D().userToPixel(new Point2D.Double(0, gauge.getY())).getY(), 2 * gauge.getRadius(), 2 * gauge.getRadius());
+		g2dPart = getBuffer().createGraphics();
+		g2dPart.translate(-gauge.getWindow2D().userToPixel(new Point2D.Double(gauge.getX(), 0)).getX() + gauge.getRadius(), -gauge.getWindow2D().userToPixel(new Point2D.Double(0, gauge.getY())).getY() + gauge.getRadius());
+	}
+
+	/**
+	 * get the graphics context of the buffered image to paint. the graphics is
+	 * already translate for the gauge projection
+	 * 
+	 * @return graphics context
+	 */
+	public Graphics2D getGraphics() {
+		return g2dPart;
+	}
+
+}
