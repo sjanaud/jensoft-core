460b9c7d6210310dfa5988eb46c5df89788a90b1
seb <seb@mycompany.com>
2013-12-26 19:14:55 +0000

    clean code
    git-svn-id: http://svn.jensoft.org/trunk/jensoft-core@3828 83d0873e-991a-40c5-9a17-dc84bc7ed2a0

--- src/main/java/com/jensoft/core/plugin/gauge/core/bg/BackgroundGaugePainter.java
+++ src/main/java/com/jensoft/core/plugin/gauge/core/bg/BackgroundGaugePainter.java
@@ -9,10 +9,27 @@ import java.awt.Graphics2D;
 
 import com.jensoft.core.plugin.gauge.core.RadialGauge;
 
+
+/**
+ * <code>BackgroundGaugePainter</code> takes the responsibility to paint gauge background.
+ * 
+ * <p>
+ * background painting process is after the envelop painting and just before gauge metrics path
+ * </p>
+ * 
+ * @since 1.0
+ * @author sebastien janaud
+ *
+ */
 public abstract class BackgroundGaugePainter {
 
+	
+	/**
+	 * implements this method to create background painter
+	 * @param g2d
+	 * @param radialGauge
+	 */
     public abstract void paintBackground(Graphics2D g2d, RadialGauge radialGauge);
-
 
 
 }
