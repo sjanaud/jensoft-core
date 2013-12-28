d0c05f00d079d08bd44176d1db7c2e4d408d7f0c
Sébastien Janaud <sebastien.janaud@gmail.com>
2013-12-28 20:47:01 +0000

    capacity core
    git-svn-id: http://svn.jensoft.org/trunk/jensoft-core@3859 83d0873e-991a-40c5-9a17-dc84bc7ed2a0

--- src/main/java/com/jensoft/core/plugin/capacity/CapacityPlugin.java
+++ src/main/java/com/jensoft/core/plugin/capacity/CapacityPlugin.java
@@ -6,7 +6,6 @@
 package com.jensoft.core.plugin.capacity;
 
 import java.awt.BasicStroke;
-import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
@@ -20,81 +19,189 @@ import com.jensoft.core.plugin.AbstractPlugin;
 import com.jensoft.core.view.View2D;
 import com.jensoft.core.window.WindowPart;
 
+/**
+ * <code>CapacityPlugin</code> draw capacity cell grouped in unit.
+ * 
+ * <p>
+ * this layer is build on a row and column count that define a total number of
+ * cell which is the total capacity.
+ * </p>
+ * <p>
+ * Unit is registered on the capacity with a given unit capacity count.
+ * <p>
+ * <p>
+ * An exception is thrown if a unit is exceeded total capacity count,
+ * </p>
+ * 
+ * @since 1.0
+ * @author sebastien janaud
+ * 
+ */
 public class CapacityPlugin extends AbstractPlugin {
 
-	private int cellWidthNumber;
-	private int cellHeightNumber;
-	private double capacityWidth;
-	private double capacityHeight;;
+	/** capacity column count */
+	private int col;
 
-	public CapacityPlugin(int cellWidthNumber, int cellHeightNumber) {
-		this.cellWidthNumber = cellWidthNumber;
-		this.cellHeightNumber = cellHeightNumber;
+	/** capacity row count */
+	private int row;
+
+	/** capacity cell width */
+	private double cellWidth;
+
+	/** capacity cell height */
+	private double cellHeight;
+
+	/** capacity with horizontal or vertical pattern */
+	private CapacityMode mode = CapacityMode.Horizontal;
+
+	/** set of unit */
+	private List<CapacityUnit> units = new ArrayList<CapacityUnit>();
+
+	/**
+	 * define the capacity pattern, horizontal or vertical
+	 * 
+	 * @author sebastien janaud
+	 * 
+	 */
+	public enum CapacityMode {
+		Horizontal, Vertical;
+	}
+
+	/**
+	 * create capacity for total (row * column) cells
+	 * 
+	 * @param row
+	 * @param col
+	 */
+	public CapacityPlugin(int row, int col) {
+		this.col = col;
+		this.row = row;
 		setFractionalMetrics(Fractional.On);
 		setAntialiasing(Antialiasing.On);
-
 	}
 
-	private List<CapacityUnit> capacities = new ArrayList<CapacityUnit>();
-
-	public void registerCapacity(CapacityUnit capacity) {
-		capacities.add(capacity);
+	/**
+	 * @return the mode
+	 */
+	public CapacityMode getMode() {
+		return mode;
 	}
 
-	private void paintCapacity(Graphics2D g2d, CapacityUnit capacity) {
-		for (CapacityCell cell : capacity.getCells()) {
+	/**
+	 * @param mode
+	 *            the mode to set
+	 */
+	public void setMode(CapacityMode mode) {
+		this.mode = mode;
+	}
+
+	/**
+	 * @return the col
+	 */
+	public int getCol() {
+		return col;
+	}
+
+	/**
+	 * @return the row
+	 */
+	public int getRow() {
+		return row;
+	}
+
+	/**
+	 * register a unit in this capacity
+	 * 
+	 * @param capacityUnit
+	 */
+	public void registerCapacity(CapacityUnit capacityUnit) {
+		// check start index
+		int startIndex = 0;
+		for (CapacityUnit unit : units) {
+			int count = unit.getCount();
+			startIndex = startIndex + count;
+		}
+		if (startIndex + capacityUnit.getCount() > col * row)
+			throw new IllegalArgumentException("unit " + capacityUnit.getCount() + ", index out of total capacity. max capacity " + row * col + " cells, unit use index range [" + startIndex + "," + (startIndex + capacityUnit.getCount()) + "]");
+		capacityUnit.setHost(this);
+		units.add(capacityUnit);
+		createIndex();
+	}
+
+	/**
+	 * paint unit
+	 * 
+	 * @param g2d
+	 * @param unit
+	 * 
+	 */
+	private void paintUnit(Graphics2D g2d, CapacityUnit unit) {
+		for (CapacityCell cell : unit.getCells()) {
 			Point2D p2d = capacityToPixel(cell);
-			Rectangle2D cellUnit = new Rectangle2D.Double(p2d.getX(), p2d.getY(), capacityWidth, capacityHeight);
-			g2d.setColor(new Alpha(capacity.getColor(),200));
+			Rectangle2D cellUnit = new Rectangle2D.Double(p2d.getX(), p2d.getY(), cellWidth, cellHeight);
+			g2d.setColor(new Alpha(unit.getColor(), 200));
 			g2d.fill(cellUnit);
 			g2d.setStroke(new BasicStroke(0.6f));
-			g2d.setColor(new Alpha(capacity.getColor(),255));
+			g2d.setColor(new Alpha(unit.getColor().darker(), 200));
+			// g2d.setColor(Color.BLACK);
 			g2d.draw(cellUnit);
 		}
 	}
 
+	/**
+	 * get the cell left corner coordinate point in device coordinate
+	 * 
+	 * @param cell
+	 * @return
+	 */
 	private Point2D capacityToPixel(CapacityCell cell) {
-		Point2D p2d = new Point2D.Double(new Double(cell.getColumn()) * capacityWidth, new Double(cell.getRow()) * capacityHeight);
+		Point2D p2d = new Point2D.Double(new Double(cell.getColumn()) * cellWidth, new Double(cell.getRow()) * cellHeight);
 		return p2d;
 	}
 
 	public int cellToVIndex(CapacityCell cell) {
-		return (cell.getColumn() + 1) * cellHeightNumber - 1;
-	}
-
-	public CapacityCell vIndexToCell(int vIndex) {
-		int[] rc = new int[2];
-		rc[0] = vIndex / cellHeightNumber;
-		rc[1] = vIndex % cellHeightNumber;
-		return new CapacityCell(rc[1], rc[0]);
+		return (cell.getColumn() + 1) * row - 1;
 	}
 
 	public int cellToHIndex(CapacityCell cell) {
-		return (cell.getRow() + 1) * cellWidthNumber - 1;
+		return (cell.getRow() + 1) * col - 1;
 	}
 
-	public CapacityCell hIndexToCell(int hIndex) {
-		int[] rc = new int[2];
-		rc[0] = hIndex / cellWidthNumber;
-		rc[1] = hIndex % cellWidthNumber;
-		return new CapacityCell(rc[0], rc[1]);
+	/**
+	 * create cell in each capacity unit
+	 */
+	private void createIndex() {
+		int startIndex = 0;
+		for (CapacityUnit unit : units) {
+			int count = unit.getCount();
+			unit.setStartIndex(startIndex);
+			startIndex = startIndex + count;
+		}
 	}
 
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * com.jensoft.core.plugin.AbstractPlugin#paintPlugin(com.jensoft.core.view
+	 * .View2D, java.awt.Graphics2D, com.jensoft.core.window.WindowPart)
+	 */
 	@Override
 	protected void paintPlugin(View2D v2d, Graphics2D g2d, WindowPart windowPart) {
 
 		if (windowPart != WindowPart.Device) {
 			return;
 		}
-
 		int width = getWindow2D().getDevice2D().getDeviceWidth();
 		int height = getWindow2D().getDevice2D().getDeviceHeight();
+		cellWidth = new Double(width) / new Double(col);
+		cellHeight = new Double(height) / new Double(row);
 
-		capacityWidth = new Double(width) / new Double(cellWidthNumber);
-		capacityHeight = new Double(height) / new Double(cellHeightNumber);
+		createIndex();
 
-		for (CapacityUnit c : capacities) {
-			paintCapacity(g2d, c);
+		for (CapacityUnit unit : units) {
+			unit.buildUnit();
+			paintUnit(g2d, unit);
 		}
 
 	}
