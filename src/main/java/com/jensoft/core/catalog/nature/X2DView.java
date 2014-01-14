2f106b035a0777d792440712fa245cf5de05aff7
Sébastien Janaud <sebastien.janaud@gmail.com>
2014-01-14 14:03:06 +0000

    x2d view
    git-svn-id: http://svn.jensoft.org/trunk/jensoft-core@4186 83d0873e-991a-40c5-9a17-dc84bc7ed2a0

---
+++ src/main/java/com/jensoft/core/catalog/nature/X2DView.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) JenSoft API
+ * This source file is part of JenSoft API, All rights reserved.
+ * JENSOFT PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.jensoft.core.catalog.nature;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import com.jensoft.core.catalog.nature.Dim;
+import com.jensoft.core.view.background.NoBackground;
+
+/**
+ * <code>X2DView</code>
+ * 
+ * @author Sebastien Janaud
+ */
+@Documented
+@Target({ ElementType.TYPE })
+@Retention(RetentionPolicy.RUNTIME)
+public @interface X2DView {	
+	Dim dimension() default @Dim(width=600,height=400);
+	Class background() default NoBackground.class;
+}
