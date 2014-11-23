c3648042999757b6f315eba9fca31d21e18ba3ae
Sébastien Janaud <sebastien.janaud@gmail.com>
2014-11-23 06:48:44 +0000

    refacto
    git-svn-id: http://svn.jensoft.org/trunk/jensoft-core@6081 83d0873e-991a-40c5-9a17-dc84bc7ed2a0

---
+++ src/main/java/com/jensoft/core/plugin/background/DeviceNightBackground.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) JenSoft API
+ * This source file is part of JenSoft API, All rights reserved.
+ * JENSOFT PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+package com.jensoft.core.plugin.background;
+
+import java.awt.Color;
+
+/**
+ * <code>Night</code>
+ * 
+ * @author sebastien janaud
+ */
+public class DeviceNightBackground extends DeviceGradientBackgroundPlugin {
+
+    private static final Color color1 = new Color(0x202737);
+    private static final Color color2 = Color.BLACK;
+    private static final float alpha = 1f;
+
+    public DeviceNightBackground() {
+        super(color1, color2, alpha);
+        setPriority(-80);
+    }
+
+}
